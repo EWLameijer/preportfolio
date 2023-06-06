db49fc55e1471797cacdc29f748bef180fd31464
EWLameijer <eric_wubbo@hotmail.com>
2023-06-06 16:57:49 +0200

    Doing something on the network!

---
+++ src/main/java/net/ericwubbo/preportfolio/Controller.java
@@ -0,0 +1,15 @@
+package net.ericwubbo.preportfolio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Controller {
+
+    @GetMapping("message")
+    public String getMessage() {
+        return "Writing your own first full-stack project is tricky!";
+    }
+}
