56df4b730e794b70affdffdac64f46933aa5ffbc
EWLameijer <eric_wubbo@hotmail.com>
2023-06-06 16:36:46 +0200

    Initial commit

---
+++ src/main/java/net/ericwubbo/preportfolio/PreportfolioApplication.java
@@ -0,0 +1,13 @@
+package net.ericwubbo.preportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreportfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ericwubbo/preportfolio/PreportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ericwubbo.preportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
