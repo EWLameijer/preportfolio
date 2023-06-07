aba241154384e47d6a7a887b761b4d334df4333b
EWLameijer <eric_wubbo@hotmail.com>
2023-06-07 16:38:51 +0200

    simple-react app. Still CORS problem

--- front-end/preportfolio/src/App.tsx
+++ front-end/preportfolio/src/App.tsx
@@ -1,5 +1,18 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
-const App = () => <p>This is a simple React app made by Wubbo!</p>
+const App = () => {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/v1/message`)
+            .then(response => response.json())
+            .then(actualData => setMessage(actualData))
+            .catch(err => console.log(`An error has occurred: ${err.message}.`))
+    }, []);
+
+
+    return <p>{message !== "" ? `The message for you is: "${message}".` : "Your message is being loaded..."}</p>
+}
 
 export default App;
