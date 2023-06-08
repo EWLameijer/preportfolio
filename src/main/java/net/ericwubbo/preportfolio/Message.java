4a95b9fb6f8ca941973e2507000272fc504d790c
EWLameijer <eric_wubbo@hotmail.com>
2023-06-08 08:54:59 +0200

    With getters, so this runs!

--- src/main/java/net/ericwubbo/preportfolio/Message.java
+++ src/main/java/net/ericwubbo/preportfolio/Message.java
@@ -2,11 +2,19 @@ package net.ericwubbo.preportfolio;
 
 public class Message {
     private String sender;
-    
+
     private String contents;
 
     public Message(String sender, String contents) {
         this.sender = sender;
         this.contents = contents;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getContents() {
+        return contents;
     }
 }
