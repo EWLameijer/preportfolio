6ab6a8894447f61c31f0c3dcb0edb46a00a4c160
EWLameijer <eric_wubbo@hotmail.com>
2023-06-14 16:26:52 +0200

    Creating a repository

--- src/main/java/net/ericwubbo/preportfolio/Controller.java
+++ src/main/java/net/ericwubbo/preportfolio/Controller.java
@@ -1,16 +1,28 @@
 package net.ericwubbo.preportfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class Controller {
 
-    @GetMapping("message")
-    public Message getMessage() {
-        var message = new Message("Wubbo", "Writing your own first full-stack project is tricky!");
-        return message;
+    @Autowired
+    MessageRepository messageRepository;
+
+    @GetMapping("all")
+    public List<Message> getMessages() {
+        return messageRepository.findAll();
+    }
+
+    @GetMapping("seed")
+    public void seedMessage() {
+        var message = new Message("Winston Churchill", "Writing your own fourth full-stack project is still tricky!");
+        messageRepository.save(message);
     }
 }
--- src/main/java/net/ericwubbo/preportfolio/PreportfolioApplication.java
+++ src/main/java/net/ericwubbo/preportfolio/PreportfolioApplication.java
@@ -9,5 +9,4 @@ public class PreportfolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PreportfolioApplication.class, args);
 	}
-
 }
