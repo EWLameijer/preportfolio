54bb936463d00c1c073be7964f7cbb18eb87d159
EWLameijer <eric_wubbo@hotmail.com>
2023-06-15 08:44:26 +0200

    using JSON instead of a normal string

--- frontend-preportfolio/src/App.jsx
+++ frontend-preportfolio/src/App.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
 
 const App = () => {
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState({});
+    
     useEffect(() => {
         fetch(`http://localhost:8080/api/v1/message`)
             .then(response => response.json())
             .then(actualData => setMessage(actualData))
             .catch(err => console.log(`An error has occurred: ${err.message}.`))
     }, []);
-    return <p>{message !== "" ? `The message for you is: "${message}".` : "Your message is being loaded..."}</p>
+
+    return <p>{message !== null ? `Message from ${message.sender}: "${message.contents}"` : "Your message is being loaded..."}</p>
 }
 
 export default App
